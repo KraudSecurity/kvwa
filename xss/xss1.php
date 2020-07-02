ff7f8fa8de7bde53fd2aa5506c5ff074e504ccd9
master <dir@kraud.ru>
2020-07-03 00:10:23 +0300

    XSS 4

--- xss/xss1.php
+++ xss/xss1.php
@@ -3,11 +3,13 @@
 <title>Kraud Vulnerable Web Applications | XSS 1</title>
 </head>
 <body>
-
+<a href="/kvwa/">Home</a><br>
 <h1>XSS 1<h1>
 <?php
 echo $_GET['phone'];
 ?>
+
+        <hr><textarea rows="10" cols="100"><script>alert(1)</script></textarea>
 
 </body>
 </html>
--- xss/xss2.php
+++ xss/xss2.php
@@ -3,7 +3,7 @@
 <title>Kraud Vulnerable Web Applications | XSS 2 <?php  echo $_GET['phone'];  ?> </title>
 </head>
 <body>
-
+<a href="/kvwa/">Home</a><br>
 <h1>XSS 2<h1>
 
         <hr><textarea rows="10" cols="100"></title><script>alert(1)</script></textarea>
