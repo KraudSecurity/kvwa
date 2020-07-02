ec2269489cfb05a94faa123ec520dec783130a21
master <dir@kraud.ru>
2020-07-03 00:32:11 +0300

    XSS 6

--- xss/xss8.php
+++ xss/xss8.php
@@ -1,21 +1,21 @@
 <html>
 <head>
-<title>Kraud Vulnerable Web Applications | XSS 5 </title>
+<title>Kraud Vulnerable Web Applications | XSS 8 </title>
 </head>
 <body>
 <a href="/kvwa/">Home</a><br>
-<h1>XSS 5<h1>
+<h1>XSS 8<h1>
 
         <form action="" method="POST">
             <input name="type" value=''>
             <input type="submit">
         </form><br>
 
-        <script>
-            var name = "<?php  echo $_POST['type'];  ?>";
-        </script>
+        <textarea>
+            <?php  echo $_POST['type']; ?>
+        </textarea>
 
-        <hr><textarea rows="10" cols="100">"</script><script>alert(1)</script></textarea>
+        <hr><textarea rows="10" cols="100"><pre></textarea><script>alert(1)</script></pre></textarea>
 
 </body>
 </html>
