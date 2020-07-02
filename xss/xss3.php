d1c7fd483eb79e3d90a8b6a4e8ba740db97fdb86
master <dir@kraud.ru>
2020-07-03 00:07:02 +0300

    XSS 4

--- xss/xss3.php
+++ xss/xss3.php
@@ -3,13 +3,15 @@
 <title>Kraud Vulnerable Web Applications | XSS 3 </title>
 </head>
 <body>
-
+<a href="/kvwa/">Home</a><br>
 <h1>XSS 3<h1>
 
         <form action="" method="POST">
-            <input name="type" value="<?php  echo $_POST['type'];  ?>">
+            <input name="type" value='<?php  echo $_POST['type'];  ?>'>
             <input type="submit">
         </form><br>
+
+        <hr><textarea rows="10" cols="100">'><script>alert(1)</script></textarea>
 
 </body>
 </html>
