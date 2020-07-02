bd312186924fac08e423d86eb43c0d6dd650e76b
master <dir@kraud.ru>
2020-07-02 23:06:40 +0300

    XSS 1

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
 </head>
 <body>
 
-<h1>XSS<h1>
-<a href="xss/xss1.php?phone=1234567890">XSS1</a><br>
+<h1>XSS</h1>
+
+<a href="xss/xss1.php?phone=1234567890">XSS1 (BODY) </a><br>
+<a href='xss/xss2.php?phone=1234567890'>XSS2 (TITLE) </a><br>
 
 </body>
 </html>
---
+++ xss/xss2.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Kraud Vulnerable Web Applications | XSS 2 <?php  echo $_GET['phone'];  ?> </title>
+</head>
+<body>
+
+<h1>XSS 2<h1>
+
+</body>
+</html>
