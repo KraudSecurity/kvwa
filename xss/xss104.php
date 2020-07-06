26d30d9636ef75be402f7212dbc14a98bf28e5e7
master <dir@kraud.ru>
2020-07-06 18:13:36 +0300

    XSS

--- xss/xss104.php
+++ xss/xss104.php
@@ -1,15 +1,17 @@
 <html>
 <head>
-<title>Kraud Vulnerable Web Applications | XSS 15 </title>
+<title>Kraud Vulnerable Web Applications | XSS 104 </title>
 </head>
 <body>
 <a href="/kvwa/">Home</a><br>
-<h1>XSS 15<h1>
+<h1>XSS 104<h1>
 
         <?php
+        $jsxss="onabort,oncanplay,oncanplaythrough,ondurationchange,onemptied,onended,onerror,onloadeddata,onloadedmetadata,onloadstart,onpause,onplay,onplaying,onprogress,onratechange,onseeked,onseeking,onstalled,onsuspend,ontimeupdate,onvolumechange,onwaiting,oncopy,oncut,onpaste,ondrag,ondragend,ondragenter,ondragleave,ondragover,ondragstart,ondrop,onblur,onfocus,onfocusin,onfocusout,onchange,oninput,oninvalid,onreset,onsearch,onselect,onsubmit,onabort,onbeforeunload,onerror,onhashchange,onload,onpageshow,onpagehide,onresize,onscroll,onunload,onkeydown,onkeypress,onkeyup,altKey,ctrlKey,shiftKey,metaKey,key,keyCode,which,charCode,location,onclick,ondblclick,oncontextmenu,onmouseover,onmouseenter,onmouseout,onmouseleave,onmouseup,onmousemove,onwheel,altKey,ctrlKey,shiftKey,metaKey,button,buttons,which,clientX,clientY,detail,relatedTarget,screenX,screenY,deltaX,deltaY,deltaZ,deltaMode,animationstart,animationend,animationiteration,animationName,elapsedTime,propertyName,elapsedTime,transitionend,onerror,onmessage,onopen,ononline,onoffline,onstorage,onshow,ontoggle,onpopstate,ontouchstart,ontouchmove,ontouchend,ontouchcancel,persisted,javascript";
+        $jsxss = explode(",",$jsxss);
 
         $type = preg_replace("'<script[^>]*?>.*?</script>'si", '', $_POST['type']);
-        $type = str_replace($type,"",$type);
+        $type = str_replace($jsxss,"",$type);
         $type = str_replace (array("*","\\"), "", $type );
         $type = strip_tags($type);
         $type = htmlentities($type, ENT_QUOTES, "UTF-8");
