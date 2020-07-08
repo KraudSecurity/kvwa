810b973cefe32ef32b7676b87060f46674479747
master <dir@kraud.ru>
2020-07-08 22:39:38 +0300

    XSS

--- xss/xss19.php
+++ xss/xss19.php
@@ -7,7 +7,7 @@
 <h1>XSS 19<h1>
 
 
-        <hr><textarea rows="10" cols="100">--><script>alert(1)</script><\!--</textarea>
+        <hr><textarea rows="10" cols="100">?--><script>alert(1)</script></textarea>
         <hr>
 
         <!-- REQUEST_URI: <?php  echo $_SERVER['REQUEST_URI'];  ?> -->
