72594099dbbe50b2d538e5cefe228dc28004711b
master <dir@kraud.ru>
2020-08-13 19:24:54 +0300

    Add Store XSS 1

---
+++ xssstore/sxss1.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>Kraud Vulnerable Web Applications | SXSS 1 </title>
+</head>
+<body>
+<a href="/kvwa/">Home</a><br>
+<h1>XSS 4<h1>
+
+<?php
+
+if(isset($_POST["submit"])) {
+    $db = new SQLite3('sxss.db');
+    $db->exec("CREATE TABLE comments(id INTEGER PRIMARY KEY, msg TEXT)");
+    $db->exec("INSERT INTO comments(msg) VALUES(".$_POST['msg'].")");
+    echo "Successfully<br>";
+}
+?>
+
+        <form action="" method="GET">
+            <input name="msg">
+            <input type="submit">
+        </form><br>
+
+
+
+</body>
+</html>
