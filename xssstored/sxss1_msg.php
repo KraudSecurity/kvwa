35cd8059459d0f01db27c3f4ac3be5a0d34323de
Aleksey Starev <starev@kraud.ru>
2021-01-15 10:43:10 +0300

    Fix XSS store

--- xssstored/sxss1_msg.php
+++ xssstored/sxss1_msg.php
@@ -13,7 +13,7 @@
     $count = $db->query('SELECT count(*) FROM comments');
     $res = $db->query('SELECT * FROM comments LIMIT 20');
 
-    $c = json_encode($count);
+    $c = json_encode($count->fetchArray());
 
     print "Total: $c <br>";
 
