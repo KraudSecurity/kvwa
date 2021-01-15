645271051730831f59de1cb14aef8227a257ab7e
Aleksey Starev <starev@kraud.ru>
2021-01-15 10:37:13 +0300

    Fix XSS store

--- xssstored/sxss1_msg.php
+++ xssstored/sxss1_msg.php
@@ -13,7 +13,7 @@
     $count = $db->query('SELECT count(*) FROM comments');
     $res = $db->query('SELECT * FROM comments LIMIT 20');
 
-    print "Total: $count<br>";
+    print "Total: $count[0] <br>";
 
     echo "<table align='center' width='70%' border='1' cellspacing='1' cellpadding='10'><tr><td width='10' align='center'>#</td><td align='center'>Message</td></tr>";
     while ($row = $res->fetchArray()) {
