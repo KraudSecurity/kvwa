8567ecceba7178451c8a635eb054e6fb025bf080
Aleksey Starev <starev@kraud.ru>
2021-01-20 11:43:31 +0300

    ADD SCHEMA JS 1

--- schema/js.php
+++ schema/js.php
@@ -8,13 +8,31 @@
 
         <a href="/kvwa/schema/js.php?from=http://127.0.0.1">TEST</a><br><br>
 
+        <a href="/kvwa/schema/js.php?from1=http://127.0.0.1">TEST</a><br><br>
+
+
+
+
                 <?php
                 $url = $_GET['from'];
                 if (!preg_match('/^javascript/',$url, $matches)) {
                     $url = "http://localhost";
                 }
+
+                $url1 = $_GET['from1'];
+                if (!preg_match('/^javascript/',$url1, $matches)) {
+                    $url1 = "http://localhost";
+                }
+
+                
+
                 ?>
                 <a href="<?php  echo $url;  ?>">Back</a>
+                <br>
+                <IMG SRC="<?php  echo $url1;  ?>">
+                <br>
+
+
 
                 <hr><textarea rows="10" cols="100">javascript:alert(1)</textarea>
                 <hr>
