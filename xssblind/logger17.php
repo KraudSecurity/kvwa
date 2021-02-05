e7f92c551df4a9f715f71fabbbebc2b98b64df6b
Aleksey Starev <starev@kraud.ru>
2021-02-05 11:40:43 +0300

    ADD XSS Blind

--- xssblind/logger17.php
+++ xssblind/logger17.php
@@ -1,1 +1,17 @@
 <?php
+class MyDB extends SQLite3
+{
+    function __construct()
+    {
+        $this->open('logger17.db');
+    }
+}
+$db = new MyDB();
+
+$res = $db->query('SELECT * FROM users LIMIT 5');
+
+print_r($res);
+
+$db->close();
+unset($db);
+?>
