6e34e2fbf15ee58714234f88df4a170725c549fa
Aleksey Starev <starev@kraud.ru>
2021-02-05 11:44:19 +0300

    ADD XSS Blind

--- xssblind/logger17.php
+++ xssblind/logger17.php
@@ -1,4 +1,8 @@
 <?php
+
+
+echo "<h1>Control Center</h1>";
+
 class MyDB extends SQLite3
 {
     function __construct()
@@ -10,7 +14,11 @@ $db = new MyDB();
 
 $res = $db->query('SELECT * FROM users LIMIT 5');
 
-print_r($res);
+echo "<table align='center' width='70%' border='1' cellspacing='1' cellpadding='10'><tr><td width='10' align='center'>#</td><td align='center'>Users</td></tr>";
+while ($row = $res->fetchArray()) {
+    echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td><td>".$row[3]."</td><td>".$row[4]."</td><td>".$row[5]."</td><td>".$row[6]."</td></tr>";
+}
+echo "</table>";
 
 $db->close();
 unset($db);
