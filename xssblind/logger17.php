e1bd188c7e6dfd5468d38debc1596331848c3345
Aleksey Starev <starev@kraud.ru>
2021-02-05 12:26:26 +0300

    ADD XSS Blind

--- xssblind/logger17.php
+++ xssblind/logger17.php
@@ -27,7 +27,7 @@ echo "<table align='center' width='70%' border='1' cellspacing='1' cellpadding='
 </tr>";
 while ($row = $res->fetchArray()) {
     $id = $row[0];
-    echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td><td>".$row[3]."</td><td>".$row[4]."</td><td>".$row[5]."</td><td>".$row[6]."</td></tr>";
+    echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td><td>".$row[3]."</td><td>".$row[4]."</td><td>".$row[5]."</td><td>".$row[6]."</td><td>".$row[7]."</td></tr>";
 }
 echo "</table>";
 
