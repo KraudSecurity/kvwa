bbfc3e71f83501f6cfd722e94236d8c812c6c5cd
Aleksey Starev <starev@kraud.ru>
2021-03-02 18:49:25 +0300

    LFI 1

---
+++ lfi/index.php
@@ -0,0 +1,21 @@
+
+<html>
+<head>
+    <title>Kraud Vulnerable Web Applications | LFI 1</title>
+</head>
+<body>
+<a href="/kvwa/">Home</a><br>
+<h1>LFI 1<h1>
+
+        <?php
+        $file = rawurldecode($_REQUEST['file']);
+        $file = preg_replace('/^.+[\\\\\\/]/', $file);
+        include("./1/{$file}");
+        ?>
+
+        <a href="index.php?file=1.php">1.php</a>
+
+        <hr><textarea rows="10" cols="100"><script>alert(1)</script></textarea>
+
+</body>
+</html>
